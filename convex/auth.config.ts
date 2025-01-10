38034d0a3c362d233930d3e255dba2b81358e09a
abhishek-akp <abhishekkumarpandey2803@gmail.com>
2025-01-10 08:55:35 -0500

    progress

---
+++ convex/auth.config.ts
@@ -0,0 +1,10 @@
+//https://musical-buffalo-17.clerk.accounts.dev
+const authConfig = {
+    providers: [
+      {
+        domain: "https://musical-buffalo-17.clerk.accounts.dev",
+        applicationID: "convex",
+      },
+    ]
+  };
+export default authConfig;
